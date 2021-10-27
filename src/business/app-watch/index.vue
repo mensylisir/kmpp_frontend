52a622e38ce4ddd4a009b5af7b5b9d9a8040047b
cuihongjuan <cuihongjuan@rdev.tech>
2021-10-27 16:12:01 +0800

    应用监控

---
+++ src/business/app-watch/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <layout-content header="" class="app-watch">
+    <div class="top">
+      <div style="display: flex; align-items: center">
+        <div class="detail">应用资源监控</div>
+        <el-menu
+          :default-active="activeKey"
+          mode="horizontal"
+          @select="tabChange"
+        >
+          <el-menu-item index="recommend"> 用量排行 </el-menu-item>
+          <el-menu-item index="useCondition"> 使用情况 </el-menu-item>
+        </el-menu>
+      </div>
+    </div>
+    <div class="bottom">
+      <router-view ></router-view>
+    </div>
+  </layout-content>
+</template>
+
+<script>
+import LayoutContent from "@/components/layout/LayoutContent";
+// import { listNamespace } from "@/api/cluster/namespace";
+
+export default {
+  name: "appWatch",
+  components: { LayoutContent },
+  data() {
+    return {
+      clusterList: [],
+      nameSpace: [],
+      count: 0,
+    };
+  },
+  methods: {
+    tabChange(index) {
+      this.activeKey = index;
+      if (index === "recommend") {
+        // 用量排行
+        this.$router.push({ name: "recommend" });
+      } else if (index === "useCondition") {
+        // 使用情况
+        this.$router.push({ name: "useCondition" });
+      }
+    },
+
+    // 获取namespace
+    // getNamespace(name) {
+    //   listNamespace(name)
+    //     .then((data) => {
+    //       this.count++;
+    //       let result = data.items || [];
+    //       result.forEach(item => {
+    //         item.cluster = name
+    //       })
+    //       this.nameSpace = this.nameSpace.concat(result);
+    //       if (this.count === this.clusterList.length) {
+    //         this.tabChange(this.activeKey);
+    //       }
+    //     })
+    //     .catch(() => {});
+    // },
+  },
+  created() {
+    this.tabChange("recommend")
+  },
+  watch: {
+    $route() {},
+  },
+  computed: {
+    activeKey: {
+      get: function () {
+        let value = this.$route.meta.activeKey;
+        return value;
+      },
+      set: function () {},
+    },
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+.app-watch {
+  background: #f4f5f7;
+  padding: 0;
+  position: relative;
+  .top {
+    background: white;
+    padding: 0 16px 0 24px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 1px 0 0 #e4e7f0;
+  }
+  .bottom {
+    height: calc(100% - 56px);
+    width: 100%;
+    .card-group {
+      display: flex;
+      .item {
+        min-width: 329px;
+        background: white;
+        padding: 24px;
+        display: flex;
+        svg {
+          width: 48px;
+          height: 48px;
+          margin-right: 16px;
+        }
+        .right {
+          display: flex;
+          flex-flow: column;
+          h5 {
+            font-size: 16px;
+            color: #2c2e33;
+            line-height: 24px;
+            font-weight: 500;
+          }
+          .description {
+            font-size: 12px;
+            color: #4b5059;
+            line-height: 20px;
+            font-weight: 400;
+            margin: 8px 0;
+            span {
+              margin-top: 8px;
+              background: #4b5059;
+            }
+          }
+          .status {
+            font-size: 12px;
+            color: #2c2e33;
+            line-height: 20px;
+            font-weight: 400;
+          }
+        }
+      }
+    }
+  }
+}
+.detail {
+  font-size: 16px;
+  color: #2c2e33;
+  line-height: 24px;
+  font-weight: 500;
+  border-right: 1px solid #e4e7f0;
+  height: 24px;
+  padding-right: 12px;
+}
+/deep/ .el-menu-item {
+  height: 56px;
+  font-size: 16px;
+  color: #2c2e33;
+}
+</style>
---
+++ src/business/app-watch/recommend.vue
@@ -0,0 +1,296 @@
+<template>
+  <div class="recommend">
+    <div class="top">
+      <h4>用量排行列表</h4>
+      <div>
+        <!-- <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in sortOption"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select> -->
+        <!-- <el-button icon="el-icon-edit" @click="sort"></el-button>
+        <el-button icon="el-icon-refresh-right" @click="reset"></el-button> -->
+      </div>
+    </div>
+    <div>
+      <el-table
+        :data="tableData"
+        :default-sort="{ prop: 'date', order: 'descending' }"
+      >
+        <el-table-column prop="namespace" label="项目">
+          <template slot-scope="scope">
+            <span style="color: #5354bb">{{
+              scope.row.namespace || "--"
+            }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="cluster" label="所属集群" width="150">
+        </el-table-column>
+        <el-table-column prop="name" label="状态" width="100">
+          <template slot-scope="scope">
+            <span>{{ scope.row.status || "--" }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="cpuCore" label="CPU使用量" sortable width="150">
+          <template slot-scope="scope">
+            <div>{{ fixData(scope.row.cpuCore) }}Core</div>
+            <span class="bottom-text"
+              >配额：{{ fixData(scope.row.cpuQuota) }}</span
+            >
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="memoryUse"
+          label="内存使用量"
+          sortable
+          width="150"
+        >
+          <template slot-scope="scope">
+            <div>{{ fixData(scope.row.memoryUse) }}Gi</div>
+            <span class="bottom-text"
+              >配额：{{ fixData(scope.row.memoryQuota) }}</span
+            >
+          </template>
+        </el-table-column>
+        <el-table-column prop="storage" label="存储使用量" sortable width="150">
+          <template slot-scope="scope">
+            <span>{{ fixData(scope.row.storage) }}Gi</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="container1"
+          label="容器组数量"
+          sortable
+          width="120"
+        >
+          <template slot-scope="scope">
+            <span>{{ scope.row.container1 || "--" }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="networkthrout"
+          label="网络流出速率"
+          sortable
+          width="150"
+        >
+          <template slot-scope="scope">
+            <span>{{ fixData(scope.row.networkthrout) }}Mbps</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="networkflow"
+          label="网络流入速率"
+          sortable
+          width="150"
+        >
+          <template slot-scope="scope">
+            <span>{{ fixData(scope.row.networkflow) }}Mbps</span>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="block">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="page.currentPage"
+          :page-sizes="page.size"
+          :page-size="page.currSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="tableData.length"
+        >
+        </el-pagination>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getTableParam } from "@/api/appWatch";
+import { searchClusters } from "@/api/cluster";
+
+export default {
+  name: "MiddleStore",
+  components: {},
+  data() {
+    return {
+      page: {
+        currentPage: 1,
+        currSize: 10,
+        size: [10, 20, 30, 40],
+      },
+      totalData: [],
+      tableData: [],
+      // CPU占用核心数,CPU配额数,内存占用,内存配额,容器组数量,网络流入量,单位M,网络流出量,单位M,存储
+      queryParams: [
+        {
+          attribute: "cpuCore",
+          value:
+            "sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests{namespace=~'.*'}) by (namespace)",
+        },
+        {
+          attribute: "cpuQuota",
+          value:
+            "sum(cluster:namespace:pod_cpu:active:kube_pod_container_resource_requests{namespace=~'.*'}) by (namespace) ",
+        },
+        {
+          attribute: "memoryUse",
+          value:
+            "sum(cluster:namespace:pod_memory:active:kube_pod_container_resource_requests{namespace=~'.*'}) by (namespace) / 2014 / 1024 / 1024",
+        },
+        {
+          attribute: "memoryQuota",
+          value:
+            "sum(cluster:namespace:pod_memory:active:kube_pod_container_resource_limits{namespace=~'.*'}) by (namespace)/ 2014 / 1024 / 1024",
+        },
+        {
+          attribute: "container1",
+          value:
+            "count(node_namespace_pod:kube_pod_info:{namespace=~'.*'}) by (namespace)",
+        },
+        {
+          attribute: "networkflow",
+          value:
+            "sum(irate(container_network_receive_bytes_total{namespace=~'.*'}[5m]) > 0) by (namespace) / 1024 / 1024",
+        },
+        {
+          attribute: "networkthrout",
+          value:
+            "sum(irate(container_network_transmit_bytes_total{namespace=~'.*'}[5m]) > 0)by (namespace) / 1024 / 1024",
+        },
+        {
+          attribute: "storage",
+          value:
+            "sum(kube_persistentvolumeclaim_resource_requests_storage_bytes{namespace=~'.*'}) by (namespace) / 1024 / 1024 / 1024",
+        },
+      ],
+    };
+  },
+  props: [""],
+  methods: {
+    // 格式化数据
+    fixData(val) {
+      let result = "--";
+      if (val) {
+        result = val.toFixed(2);
+      }
+      return result;
+    },
+    // 数据分页
+    pageration() {
+      this.tableData = this.totalData.filter((item, index) => {
+        return (
+          this.page.currSize * (this.page.currentPage - 1) <= index &&
+          index < this.page.currSize * this.page.currentPage
+        );
+      });
+      console.log(this.tableData);
+    },
+    handleSizeChange(val) {
+      this.page.currSize = val;
+      this.page.currentPage = 1;
+      this.pageration();
+    },
+    handleCurrentChange(val) {
+      this.page.currentPage = val;
+      this.pageration();
+    },
+    sort() {},
+    reset() {},
+    // 获取集群信息
+    getCluester() {
+      searchClusters(1, 1000, "")
+        .then((data) => {
+          this.clusterList = data.items.filter((item) => {
+            return item.source === "local";
+          });
+          this.getTableData();
+        })
+        .catch(() => {});
+    },
+    // 获取表格数据
+    getTableData() {
+      this.clusterList.forEach((item) => {
+        this.queryParams.forEach((params) => {
+          this.getTableParam(item.name, params);
+        });
+      });
+    },
+    // 获取列表
+    getTableParam(cluster, params) {
+      getTableParam(cluster, params.value)
+        .then((data) => {
+          const result = data.data.result || [];
+          result.forEach((item) => {
+            let index = this.totalData.findIndex((item1) => {
+              return (
+                item1.namespace === item.metric.namespace &&
+                item1.cluster === cluster
+              );
+            });
+            if (index === -1) {
+              let tableItem = {
+                namespace: item.metric.namespace,
+                cluster: cluster,
+              };
+              tableItem[params.attribute] = Number(item.value[1]);
+              this.totalData.push(tableItem);
+            } else {
+              this.totalData[index][params.attribute] = Number(item.value[1]);
+            }
+          });
+          this.pageration();
+        })
+        .catch(() => {});
+    },
+  },
+  created() {
+    this.getCluester();
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+.recommend {
+  background: white;
+  padding: 24px;
+  margin-top: 8px;
+  height: calc(100% - 48px);
+  .top {
+    padding: 0 0 13px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h4 {
+      font-size: 16px;
+      color: #2c2e33;
+      line-height: 24px;
+      font-weight: 500;
+    }
+    .sort {
+      margin: 0 16px 0 8px;
+    }
+  }
+  .block {
+    text-align: right;
+    margin: 15px 0;
+  }
+  /deep/ table {
+    td {
+      font-size: 14px;
+      color: #2c2e33;
+      line-height: 22px;
+      font-weight: 400;
+    }
+  }
+  .bottom-text {
+    font-size: 12px;
+    color: #797f8c;
+    line-height: 20px;
+    font-weight: 400;
+  }
+}
+</style>
