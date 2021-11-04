ac5948722a44ed57f507cfdeba1527dc63154de3
cuihongjuan <cuihongjuan@rdev.tech>
2021-11-04 14:14:33 +0800

    修复缺陷

---
+++ src/api/appWarning.js
@@ -0,0 +1,20 @@
+import {get, post, del} from "@/plugins/request"
+
+export function deploy(cluster,nameSpace,data) {
+  return post(`/proxy/kubeapps/${cluster}/api/kubeops/v1/clusters/default/namespaces/${nameSpace}/releases`, data)
+}
+
+export function deleteMiddle(cluster,nameSpace,name) {
+  return del(`/proxy/kubeapps/${cluster}/api/kubeops/v1/clusters/default/namespaces/${nameSpace}/releases/${name}?purge=true`)
+}
+
+// CPU
+export function getTableData(cluster) {
+  return get(`/proxy/kubernetes/${cluster}/api/v1/namespaces/monitoring/services/prometheus-k8s:web/proxy/api/v1/alerts`)
+}
+
+export function getCPUParam(cluster,params,start,end,step) {
+  return get(`/proxy/kubernetes/${cluster}/api/v1/namespaces/monitoring/services/prometheus-k8s:web/proxy/api/v1/query_range?query=${params}&start=${start}&end=${end}&step=${step}`)
+}
+
+
