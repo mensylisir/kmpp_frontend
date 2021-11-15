0f46077ac443dbfc0a28c65a9e93bff80ad1c459
cuihongjuan <cuihongjuan@rdev.tech>
2021-11-15 15:18:38 +0800

    注释多余功能

--- src/business/xterm/index.vue
+++ src/business/xterm/index.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
-    <div style="position: absolute;right: 10px;z-index: 200">
-      <el-button size="large" v-if="isRun" icon="el-icon-video-pause" @click="changeMode()" style="float: right;"></el-button>
-      <el-button size="large" v-if="!isRun" icon="el-icon-video-play" @click="changeMode()" style="float: right;"></el-button>
+    <div style="position: absolute; right: 10px; z-index: 200">
+      <el-button
+        size="large"
+        v-if="isRun"
+        icon="el-icon-video-pause"
+        @click="changeMode()"
+        style="float: right"
+      ></el-button>
+      <el-button
+        size="large"
+        v-if="!isRun"
+        icon="el-icon-video-play"
+        @click="changeMode()"
+        style="float: right"
+      ></el-button>
     </div>
     <div>
       <div id="terminal-container"></div>
@@ -11,9 +23,9 @@
 </template>
 
 <script>
-import "xterm/css/xterm.css"
-import { Terminal } from "xterm"
-import { getProvisionerLog, getClusterLog } from "@/api/cluster"
+import "xterm/css/xterm.css";
+import { Terminal } from "xterm";
+import { getProvisionerLog, getClusterLog } from "@/api/cluster";
 
 export default {
   name: "xTerm",
@@ -21,76 +33,88 @@ export default {
     return {
       term: Terminal,
       isRun: true,
-    }
+    };
   },
   methods: {
     init() {
-      let terminalContainer = document.getElementById("terminal-container")
+      let terminalContainer = document.getElementById("terminal-container");
       this.term = new Terminal({
+        rendererType: "canvas", //渲染类型
         disableStdin: true,
-        cursorStyle: "bar",
+        cursorStyle: "underline",
         cols: 100,
         rows: 58,
         letterSpacing: 1,
         fontSize: 12,
-      })
-
-      this.term.open(terminalContainer)
-      this.term.write("connect to logger...")
+        theme: {
+          foreground: "#ECECEC", //字体
+          background: "#000000", //背景色
+          cursor: "help", //设置光标
+          lineHeight: 20,
+        },
+      });
+      this.term.open(terminalContainer);
+      this.term.write("connect to logger...");
       setTimeout(() => {
-        this.term.clear()
-      }, 3000)
-      const clusterName = this.$route.query.clusterName
-      const logId = this.$route.query.logId
+        this.term.clear();
+      }, 3000);
+      const clusterName = this.$route.query.clusterName;
+      const logId = this.$route.query.logId;
       this.timer = setInterval(() => {
         if (this.isRun) {
           if (logId !== undefined) {
             getProvisionerLog(clusterName, logId)
               .then((data) => {
-                this.term.clear()
-                const text = data.msg.replace(/\n/g, "\r\n")
-                this.term.write(text)
+                this.term.clear();
+                const text = data.msg.replace(/\n/g, "\r\n");
+                this.term.write(text);
                 setTimeout(() => {
-                  this.term.scrollToBottom()
-                }, 100)
+                  this.term.scrollToBottom();
+                }, 100);
               })
               .catch((error) => {
-                if (error.toString() === "Error: Request failed with status code 400") {
-                  this.isRun = false
+                if (
+                  error.toString() ===
+                  "Error: Request failed with status code 400"
+                ) {
+                  this.isRun = false;
                 } else {
-                  this.term.write("no log to show" + error)
+                  this.term.write("no log to show" + error);
                 }
-              })
+              });
           } else {
             getClusterLog(clusterName)
               .then((data) => {
-                this.term.clear()
-                const text = data.msg.replace(/\n/g, "\r\n")
-                this.term.write(text)
+                this.term.clear();
+                const text = data.msg.replace(/\n/g, "\r\n");
+                this.term.write(text);
                 setTimeout(() => {
-                  this.term.scrollToBottom()
-                }, 100)
+                  this.term.scrollToBottom();
+                }, 100);
               })
               .catch((error) => {
-                if (error.toString() === "Error: Request failed with status code 400") {
-                  this.isRun = false
+                if (
+                  error.toString() ===
+                  "Error: Request failed with status code 400"
+                ) {
+                  this.isRun = false;
                 } else {
-                  this.term.write("no log to show" + error)
+                  this.term.write("no log to show" + error);
                 }
-              })
+              });
           }
         }
-      }, 5000)
+      }, 5000);
     },
     changeMode() {
-      this.isRun = !this.isRun
+      this.isRun = !this.isRun;
     },
   },
   destroyed() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
   },
   mounted() {
-    this.init()
+    this.init();
   },
-}
+};
 </script>
